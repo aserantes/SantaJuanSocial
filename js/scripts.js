27f5bec3623f863c74c03ab3831baaa0f939f49a
aserantes <aserantes@gmail.com>
2019-01-27 22:05:10 -0300

    almost there...

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,102 @@
-//required for bootstrap popover initialization
+//bootstrap popover init
 $(function() {
-    $('[data-toggle="popover"]').popover()
+	$('[data-toggle="popover"]').popover()
 })
 
 // // dispose of popovers on clicking anything other than a popover
 $('html').on('click', function(e) {
-    if (typeof $(e.target).data('original-title') == 'undefined') {$('[data-original-title]').popover('dispose');}
+	if (typeof $(e.target).data('original-title') == 'undefined') { $('[data-original-title]').popover('dispose'); }
 });
+
+
+
 
 // on click event for all spans childs of class "circle-box" (the colored circles on map), a popover is dinamically created
 $('.circle-box span').on('click', function() {
-    var lote = $(this).data("lote");
-    $(this).popover({
-        title: "Lote " + lote,
-        animation: true,
-        trigger: "focus",
-        content: "placeholder, later I'll just populate this with database query of house number " + lote,
-        placement: "auto",
-    }).popover('show');
+	var lote = $(this).data("lote");
+	$(this).popover({
+		title: "Lote " + lote,
+		animation: true,
+		trigger: "focus",
+		content: "placeholder, later I'll just populate this with database query of house number " + lote,
+		placement: "auto",
+	}).popover('show');
 });
+
+
+
 
 var activeLi //this is just a global variable to know the active Event at any moment
 
 $(".left ul li").click(function() { //on click over any event...
-    if (activeLi !== undefined) { activeLi.removeClass('active'); } //remove active class of previous event (if any)
-    $(this).addClass('active'); //adds active class to clicked event
-    activeLi = $(this); //sets the new activeLi in global variable activeLi
-    $(".circle-box").removeClass("circle-grey circle-green circle-yellow circle-red"); //remove all circle color classes
-    $(".circle-box").addClass("circle-grey"); //reset all circles to grey
+	if (activeLi !== undefined) { activeLi.removeClass('active'); } //remove active class of previous event (if any)
+	$(this).addClass('active'); //adds active class to clicked event
+	activeLi = $(this); //sets the new activeLi in global variable activeLi
+	$(".circle-box").removeClass("circle-grey circle-green circle-yellow circle-red"); //remove all circle color classes
+	$(".circle-box").addClass("circle-grey"); //reset all circles to grey
 
-    // take name of event
-    var evento = $(this).html();
+	// aquire data from the active event li
+	var eventname = $(this).data("name");
+	var eventdate = $(this).data("date");
+	var eventnotes = $(this).data("notes");
 
-    // populate information data
-    $("#infopanel").html("Information placeholder related to the particular <b>Event</b> on mouse click. In this case, the event name is <b>" + evento + "</b>. This is now hardcoded, later when I learn database I will fill this by aquiring the data from a query");
+	//make arrays of rsvp
+	var invited = $(this).data("invited"); // array of invited houses
+	var confirmed = $(this).data("confirmed"); // array of confirmed houses
+	var notgoing = $(this).data("notgoing"); // array of notgoing houses
+	jQuery.each(invited, function() {
+		/* iterate through array of invited*/
+		$(".circle-l" + this).removeClass("circle-grey circle-green circle-yellow circle-red") //remove all circle color classes
+			.addClass("circle-yellow"); // add yellow circle class 
+	});
+	jQuery.each(confirmed, function() {
+		/* iterate through array of confirmed*/
+		$(".circle-l" + this).removeClass("circle-grey circle-green circle-yellow circle-red") //remove all circle color classes
+			.addClass("circle-green"); // add green circle class
+	});
+	jQuery.each(notgoing, function() {
+		/* iterate through array of notgoing*/
+		$(".circle-l" + this).removeClass("circle-grey circle-green circle-yellow circle-red") //remove all circle color classes
+			.addClass("circle-red"); // add red circle class
+	});
 
-    //make arrays of rsvp
-    var invited = $(this).data("invited"); // array of invited houses
-    var confirmed = $(this).data("confirmed"); // array of confirmed houses
-    var notgoing = $(this).data("notgoing"); // array of notgoing houses
-    jQuery.each(invited, function() {
-        /* iterate through array */
-        $(".circle-l" + this).removeClass("circle-grey circle-green circle-yellow circle-red") //remove all circle color classes
-            .addClass("circle-yellow"); // add yellow circle class
-    });
-    jQuery.each(confirmed, function() {
-        /* iterate through array*/
-        $(".circle-l" + this).removeClass("circle-grey circle-green circle-yellow circle-red") //remove all circle color classes
-            .addClass("circle-green"); // add green circle class
-    });
-    jQuery.each(notgoing, function() {
-        /* iterate through array*/
-        $(".circle-l" + this).removeClass("circle-grey circle-green circle-yellow circle-red") //remove all circle color classes
-            .addClass("circle-red"); // add red circle class
-    });
+	//convert arrays to string
+	var confirmedStringified = confirmed.join(', ');
+	var notgoingStringified = notgoing.join(', ');
+
+	//get quantity of elements in each array
+	var confirmedCount = $(confirmed).length;
+	var notgoingCount = $(notgoing).length;
+
+	// generate html and insert into right information panel
+	$("#infopanel").html(
+		"<b>Name: </b>" + eventname + "<br>" +
+		"<b>Date: </b>" + eventdate + "<br>" +
+		"<b>Notes: </b>" + eventnotes + "<br>" +
+		"<b>Attending: </b>" + confirmedCount + "<br>" +
+		confirmedStringified + "<br>" +
+		"<b>Not Attending: </b>" + notgoingCount + "<br>" +
+		notgoingStringified
+	);
+
+	//show buttons to edit or delete active event.
+	$("#buttonpad").css("visibility", "visible");
+
+	//show information div border.
+	$("#infopanel").css("visibility", "visible");
+});
+
+
+
+
+//tempus dominus init with options
+
+$(function() {
+	$('#datetimepicker').datetimepicker({
+		format: "DD/MM/YYYY",
+		dayViewHeaderFormat: "DD/MM/YYYY",
+		viewMode: "months",
+		minDate: "now",
+		useCurrent: false
+	});
 });
